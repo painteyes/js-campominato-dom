e7b9ca6f8a9aa0640ba4890e57633df3d42c0232
painteyes <andreantoniocchipinti@gmail.com>
2021-11-24 16:33:27 +0100

    commit changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 // Consegna
 
-// [x] Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
 
-// [] L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+// L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
 // con difficoltà 1 => tra 1 e 100
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 
-// [] Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-// [] I numeri nella lista delle bombe non possono essere duplicati.
-// [] In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-// [] La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-// [] Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. I numeri nella lista delle bombe non possono essere duplicati.
+        // [] Creo un array vuoto
+        // [] Popolo, con un ciclo while, l'array di numeri univoci nel range della difficoltà prescelta fino a quando non contiene 16 elementi
+
+// In seguito l'utente clicca su una cella: 
+    // [] Aggiungo un eventListener al click della cella
+
+        // se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
+            // [] Controllo, con l'if,  se il textContent della cella è incluso nell'array di 16 numeri
+            // [] Aggiungo la classe red alla cella
+
+        // altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+            // [] Aggiungo una classe active alla cella 
+            // [] Creo un array vuoto per contare i click azzeccati
+            // [] Aggiungo il textContent della cella cliccata
+
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+    // [] Creo una variabile con il numero massimo di tentativi possibili -> let .... = numero massimo di numeri nel range della difficoltà prescelta - il numero delle bombe (16)
+    // [] Controllo, con l'if, se la variabile === lunghezza dell'array che conta i click azzeccati
+
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+    // [] Stampo un messaggio concatenando una stringa all'array.lenght con i click azzeccati
 
 // BONUS:
 // [] 1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
